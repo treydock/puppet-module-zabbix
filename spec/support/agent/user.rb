503ee5e9dacf3b67a46d54b4ab5d612471403c5f
Trey Dockendorf <treydock@gmail.com>
2014-07-26 15:09:38 -0500

    * Use forcelocal for user/group of agent

--- spec/support/agent/user.rb
+++ spec/support/agent/user.rb
@@ -6,21 +6,23 @@ shared_context 'zabbix::agent::user' do
 
   it 'manage User[zabbix]' do
     should contain_user('zabbix').with({
-      :ensure    => 'present',
-      :comment   => 'Zabbix Monitoring System',
-      :gid       => 'zabbix',
-      :home      => '/var/lib/zabbix',
-      :shell     => '/sbin/nologin',
-      :uid       => nil,
-      :system    => 'true',
+      :ensure     => 'present',
+      :comment    => 'Zabbix Monitoring System',
+      :gid        => 'zabbix',
+      :home       => '/var/lib/zabbix',
+      :shell      => '/sbin/nologin',
+      :uid        => nil,
+      :system     => 'true',
+      :forcelocal => 'true',
     })
   end
 
   it 'manage Group[zabbix]' do
     should contain_group('zabbix').with({
-      :ensure  => 'present',
-      :gid     => nil,
-      :system  => 'true',
+      :ensure     => 'present',
+      :gid        => nil,
+      :system     => 'true',
+      :forcelocal => 'true',
     })
   end
 end
